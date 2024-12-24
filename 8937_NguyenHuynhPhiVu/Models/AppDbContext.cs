8694d11a604db9274ed194099b43b94d9ff1caad
PhiVu1409 <vu140902@gmail.com>
2024-12-25 00:21:33 +0700

    init project

---
+++ 8937_NguyenHuynhPhiVu/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using _8937_NguyenHuynhPhiVu.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using static NuGet.Packaging.PackagingConstants;
+
+namespace _8937_NguyenHuynhPhiVu.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<NganhHoc> NganhHoc { get; set; }
+        public DbSet<SinhVien> SinhVien { get; set; }
+        public DbSet<HocPhan> HocPhan { get; set; }
+        public DbSet<DangKy> DangKy { get; set; }
+        public DbSet<ChiTietDangKy> ChiTietDangKy { get; set; }
+    }
+}
---
+++ 8937_NguyenHuynhPhiVu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _8937_NguyenHuynhPhiVu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
